382ca6ad4e41ab23f52474d1c48df198356f1ec5
Robert Lynch <robert.lynch@alaskaair.com>
2025-02-26 21:18:15 -0800

    add some vscode settings

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -5,12 +5,23 @@
     enable = true;
     package = pkgs.unstable.vscodium;
 
-    extensions = with pkgs.vscode-extensions; [
+    extensions = with pkgs.unstable.vscode-extensions; [
       dracula-theme.theme-dracula
+      esbenp.prettier-vscode
+      vscode-icons-team.vscode-icons
     ];
 
     userSettings = {
       "workbench.colorTheme" = "Dracula Theme";
+      "workbench.iconTheme" = "vscode-icons";
+      "editor.formatOnPaste" = true;
+      "editor.formatOnSave" = true;
+      "[typescript]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
+      "[typescriptreact]" = {
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+      };
     };
   };
 }
